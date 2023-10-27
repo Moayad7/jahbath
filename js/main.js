52a5e85baca78a465b5dd97542212a39194d9fc6
Moayad7 <mouayad.ahmad7@gmail.com>
2023-10-28 00:12:54 +0300

    1

--- js/main.js
+++ js/main.js
@@ -23,3 +23,28 @@ function Menu2() {
   item.name === "close";
   list.classList.remove("left-[0px]");
 }
+
+
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+let mybutton2 = document.getElementById("myBtn2");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    mybutton2.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+    mybutton2.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
